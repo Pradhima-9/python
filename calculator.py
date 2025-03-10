42b31855ed9445aad0b4687e51cd2778b0de6ffd
Pradhima-9 <shinkhi99@gmail.com>
2025-03-10 18:29:23 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a=float(input("enter num1:"))
+op=input("enter operator:")
+b=float(input("enter num2:"))
+if(op=='+'):
+  print(a+b)
+elif(op=='-'):
+  print(a-b)
+elif(op=='*'):
+  print(a*b)
+elif(op=='/'):
+  print(a/b)
+else:
+  print("invalid operator")
+
---
+++ cube.py
@@ -0,0 +1,9 @@
+def say(name):
+    print("hello "+name)
+say("hi")
+def cube(n):
+    print(n)
+    return n*n
+    print(n) 
+r=cube(381)
+print(r)
