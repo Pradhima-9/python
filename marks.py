7be33cdaaaa2b79e0b1a4b60e4cc4dad9d446985
Pradhima-9 <shinkhi99@gmail.com>
2025-03-10 19:18:35 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,12 @@
+marks=float(input("enter your marks:"))
+if marks>=90:
+    print(" very good")
+elif marks>=80:
+    print("good")
+elif marks>=70:
+    print("average")
+elif marks>=60:
+    print("ok average")
+else:
+    print("below average")
+
